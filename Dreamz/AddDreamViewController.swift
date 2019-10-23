5d85319835505fb17ef66713849f378f76a964e2
Frankie36 <ngunjiri.k.francis@gmail.com>
2019-10-23 12:16:28 +0300

    Added insert dream functionality

--- Dreamz/AddDreamViewController.swift
+++ Dreamz/AddDreamViewController.swift
@@ -9,27 +9,58 @@
 import UIKit
 
 class AddDreamViewController: UIViewController {
-
+    
+    private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     @IBOutlet weak var txtTitle: UITextField!
     @IBOutlet weak var txtDescription: UITextField!
+    var dreams = [Dream]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func addDreamToDb(_ sender: UIButton) {
+        if(txtTitle.text!.isEmpty){
+            let alert = UIAlertController(title: "Title Empty", message: "The title of the dream is required", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            
+            self.present(alert, animated: true)
+            return
+        }
+        if(txtDescription.text!.isEmpty){
+            let alert = UIAlertController(title: "Description Empty", message: "The description of the dream is required", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            
+            self.present(alert, animated: true)
+            return
+        }
+        
+        let data = Dream()
+        let dream = Dream(entity: Dream.entity(), insertInto: context)
+        
+        dream.title = txtTitle.text
+        dream.details = txtDescription.text
+        appDelegate.saveContext()
+        dreams.append(dream)
+        let index = IndexPath(row:dreams.count - 1, section:0)
+        
+        self.dismiss(animated: true, completion: nil)
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }

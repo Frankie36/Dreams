f077be9eaf01b9941d5f8c40f002d0a8b8b007e1
Frankie36 <ngunjiri.k.francis@gmail.com>
2019-10-23 12:02:38 +0300

    Added 'Add Dream' interface

---
+++ Dreamz/AddDreamViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddDreamViewController.swift
+//  Dreamz
+//
+//  Created by Francis Ngunjiri on 23/10/2019.
+//  Copyright © 2019 Francis Ngunjiri. All rights reserved.
+//
+
+import UIKit
+
+class AddDreamViewController: UIViewController {
+
+    @IBOutlet weak var txtTitle: UITextField!
+    @IBOutlet weak var txtDescription: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addDreamToDb(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
